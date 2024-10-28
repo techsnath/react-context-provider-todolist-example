942aa8dbf1e8be0658a4638fb6b0efda06110092
techsnath <mail.tech.snath@gmail.com>
2024-10-28 21:12:33 +0530

    react todo list with react context provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, createContext, useContext } from 'react';
+
+const ToDoContext = createContext();
+
+const ToDoProvider = ({children}) => {
+  
+  const [task, setTask] = useState([{id : 1, title: 'This your first todo list', complete : true }]);
+  const addNewTask = (title) => {
+    
+    setTask([...task, { id : task.length + 1 , title ,  complete : false }])
+  } 
+  const completeTask = (object) => {
+    let list = [...task];
+    list.map(item => {
+      if(item.id === object.id)
+      item.complete = true;
+      return item;
+    });
+    setTask(list);
+  } 
+  return (
+    <ToDoContext.Provider value={{addNewTask, completeTask, task}}>
+      {children}
+    </ToDoContext.Provider>
+  )
+}
+
+const ToDoForm = () => {
+  const { addNewTask } = useContext(ToDoContext);
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
+  const submit = (e) => {
+    e.preventDefault();
+    if(!title || !date) {
+      return;
+    }
+    addNewTask(title);
+  }
+  return (
+    <form onSubmit={submit}>
+      <input type="text" onChange={(e)=>setTitle(e.target.value)} value={title}/>
+      <input type="datetime-local" onChange={(e)=>setDate(e.target.value)} value={date}/>
+      <button type='submit'>Add New Task</button>
+    </form>
+  
+  )
+}
+
+const ToDoList = () =>{
+  const { task, completeTask } = useContext(ToDoContext);
+  return (
+    <ul>
+      { task.map((item) => {
+        return <li key={item.id.toString()} style={{textDecoration : item.complete ? 'line-through' : 'none'}} >{item.title} <button style={{visibility : item.complete ? 'hidden' : 'visible'}} onClick={() => {completeTask(item)}} >Complete</button></li>
+      })
+      }
+    </ul>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <ToDoProvider>
+          <ToDoForm/>
+          <ToDoList/>
+        </ToDoProvider>
   );
 }
 
